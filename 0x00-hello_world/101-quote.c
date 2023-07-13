42696ce2b2594de1ce08c135bfbff6c1d64519b6
CyberGA <etomu.joshua@gmail.com>
2023-07-13 07:05:29 -0700

    prints without using printf or puts functions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, sentence, sizeof(sentence) - 1);
 	return (1);
 }
