e2850f2db955f5c7d69c2837e103af3dc16f16e5
CyberGA <etomu.joshua@gmail.com>
2023-07-14 08:37:32 -0700

    possible combination of three single digit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets in lowercase
+ * Return: (0)
+ */
+int main(void)
+{
+	int num;
+	int inside_num;
+	int inner_num;
+
+	for (num = 0; num <= 7; ++num)
+	{
+		for (inside_num = num + 1; inside_num <= 8; ++inside_num)
+		{
+			for (inner_num = inside_num + 1; inner_num <= 9; inner_num++)
+			{
+				putchar(num + '0');
+		                putchar(inside_num + '0');
+				putchar(inner_num + '0');
+				if (num != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
