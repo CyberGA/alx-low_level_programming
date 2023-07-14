9b7c35867d4af10ae8d27921c09b3983ff677a24
CyberGA <etomu.joshua@gmail.com>
2023-07-14 08:26:59 -0700

    possible combination of two single digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets in lowercase
+ * Return: (0)
+ */
+int main(void)
+{
+	int num;
+	int inside_num;
+
+	for (num = 0; num < 9; ++num)
+	{
+		for (inside_num = num + 1; inside_num <= 9; ++inside_num)
+		{
+			putchar(num + '0');
+			putchar(inside_num + '0');
+		
+			if (num != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
