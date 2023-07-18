813637f2d1d4e7226d6605cd371b764ccec35833
CyberGA <etomu.joshua@gmail.com>
2023-07-18 07:15:42 -0700

    10 times lowercase alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabet 10 times
+ * made by Gbenga Etomu
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar(ch);
+	}
+}
