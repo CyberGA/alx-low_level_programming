03d0dcea57613930b39564df85d1567c0dca93f9
CyberGA <etomu.joshua@gmail.com>
2023-07-18 08:47:26 -0700

    computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n - value to computer
+ * made by Gbenga Etomu
+ *
+ * Return: int
+ */
+int _abs(int n)
+{
+	return (n > 0 ? n : -n);
+}
