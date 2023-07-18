34c34c91c6d917cce64f82bcf716fa7673ece3eb
CyberGA <etomu.joshua@gmail.com>
2023-07-18 14:15:32 -0700

    prints all natural numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints from any numbeer to 98
+ * @n: start number
+ *
+ */
+void print_to_98(int n)
+{
+	int s;
+
+	if (n >= 98)
+	{
+		while (n >= 98)
+		{
+			_putchar(n, '0');
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n--;
+		}
+	}
+	else
+		while (n <= 98)
+		{
+			_putchar(n, '0');
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n++;
+		}
+	_putchar('\n');
+}
