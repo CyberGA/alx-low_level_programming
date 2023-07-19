d0c526044259bc8c69840f3e81851210621c9397
CyberGA <etomu.joshua@gmail.com>
2023-07-18 18:35:19 -0700

    checks for +ve, -ve or zero digits

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - cheeck if a number is +ve, -ve or 0
+ * made by Gbeng Etomu
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	positive_or_negative(0);
+	return (0);
+}
