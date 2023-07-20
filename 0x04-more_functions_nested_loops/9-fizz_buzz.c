be66e1f7298b04397b475936055821407940e730
CyberGA <etomu.joshua@gmail.com>
2023-07-20 07:41:00 -0700

    fizz-buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,15 +10,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
 			printf("Fizz");
-
-		if (i % 5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
-		
-		if (i % 5 != 0 && i % 3 != 0)
+		else
 			printf("%d", i);
-		printf(" ");
+
+		if(i != 100)
+			printf(" ");
 	}
 	printf("\n");
 }
