909843f16b760a01889a2f80408c07f40fa74131
CyberGA <etomu.joshua@gmail.com>
2023-07-24 08:04:02 -0700

    prints string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,9 +8,11 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = sizeof(s); i <= 0; i--)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		_putchar(s[i]);
 	}
+
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
 	_putchar('\n');
 }
