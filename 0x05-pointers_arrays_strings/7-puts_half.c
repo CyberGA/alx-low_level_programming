8b70536f04d49d49201cc97cd03420d1e149f06f
CyberGA <etomu.joshua@gmail.com>
2023-07-24 13:08:45 -0700

    prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to print
+ */
+void puts_half(char *str)
+{
+	int i, n;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+
+	n = (i - 1) / 2;
+
+	for (i = n + 1; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
