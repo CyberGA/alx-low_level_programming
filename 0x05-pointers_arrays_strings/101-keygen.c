b759354f2324270a5b26aa4f4324f14052313b13
CyberGA <etomu.joshua@gmail.com>
2023-07-24 14:15:11 -0700

    generates random valid passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,7 +14,7 @@ int main(void)
 	int ascii = 2772, i = 0, j, random;
 	char password[100];
 	time_t t;
-	
+
 	srand((int) time(&t));
 	while (ascii > 126)
 	{
@@ -23,14 +23,14 @@ int main(void)
 		ascii -= random;
 		i++;
 	}
-	
+
 	if (ascii > 0)
 		password[i] = ascii;
 	else
 	{
 		i--;
 	}
-	
+
 	for (j = 0; j <= i; j++)
 	{
 		printf("%c", password[j]);
