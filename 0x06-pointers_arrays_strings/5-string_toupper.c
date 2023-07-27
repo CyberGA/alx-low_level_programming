3b564230334efe145e8898be6c3075714a1834d0
CyberGA <etomu.joshua@gmail.com>
2023-07-27 09:48:50 -0700

    convert lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *string_toupper - changes all lowercase letters of a
+ * string to uppercase
+ * @c: character to uppercase
+ * Return: uppercase character
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+			c[i] = c[i] - 32;
+	}
+
+	return (c);
+}
