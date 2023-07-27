0375324386323df62a9ef7d8ccb0689554fef71b
CyberGA <etomu.joshua@gmail.com>
2023-07-27 05:54:53 -0700

    concatenate string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+}
