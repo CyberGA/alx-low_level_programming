36a80bfe2baadf73bd4b78785a2f40ba685c4f8d
CyberGA <etomu.joshua@gmail.com>
2023-07-27 06:18:47 -0700

    concatenate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,4 +8,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	int dest_len, src_len;
+
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+	{
+	}
+
+	for (src_len = 0; src[src_len] != '\0'; src_len++)
+	{
+		dest[dest_len + src_len] = src[src_len];
+	}
+	return (dest);
 }
