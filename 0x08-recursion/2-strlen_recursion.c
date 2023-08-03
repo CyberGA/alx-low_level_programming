d5530246a136a5490ef737482e0def5de1787603
CyberGA <etomu.joshua@gmail.com>
2023-08-02 18:56:24 -0700

    made correction

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,12 +7,7 @@
  */
 int _strlen_recursion(char *s)
 {
-	unsigned int len = 0;
-
 	if (*s == '\0')
 		return (0);
-	len++;
-	_strlen_recursion(s + 1);
-
-	return (len);
+	return(1 + _strlen_recursion(s + 1));
 }
