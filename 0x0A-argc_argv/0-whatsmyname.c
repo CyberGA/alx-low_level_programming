c4dd243a016f0433b22fb39e4265e07c334e08ac
CyberGA <etomu.joshua@gmail.com>
2023-08-04 17:26:53 -0700

    prints the name of a program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Success (0)
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
