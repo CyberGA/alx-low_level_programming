6ab1f7212f118d6d8bef146574548eb858d002f1
CyberGA <etomu.joshua@gmail.com>
2023-08-08 09:45:12 -0700

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,15 +13,17 @@ char *str_concat(char *s1, char *s2)
 	char *res;
 	int i, j, total, s1_len = 0, s2_len = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		s1_len++;
-	}
+	if (s1 != NULL)
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			s1_len++;
+		}
 
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		s2_len++;
-	}
+	if (s2 != NULL)
+		for (j = 0; s2[j] != '\0'; j++)
+		{
+			s2_len++;
+		}
 
 	total = s1_len + s2_len + 1;
 	res = malloc(total * sizeof(char));
@@ -31,15 +33,11 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; i < s1_len; i++)
 	{
-		if (s1 == NULL)
-			break;
 		res[i] = s1[i];
 	}
 
 	for (j = 0; j < s2_len; j++)
 	{
-		if (s2 == NULL)
-			break;
 		res[i] = s2[j];
 		i++;
 	}
