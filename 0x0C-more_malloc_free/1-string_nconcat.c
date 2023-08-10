cfc8d31b9bca0283f1fa36ad37f8dea3e5830d56
CyberGA <etomu.joshua@gmail.com>
2023-08-10 08:08:43 -0700

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,10 +14,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, k = 0, len = 0;
 	char *res;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1 != NULL && s1[i] != '\0'; i++)
 		len++;
 
-	for (j = 0; s2[j] != '\0'; j++)
+	i = 0;
+
+	for (j = 0; s2 != NULL && s2[j] != '\0'; j++)
 	{
 	}
 
@@ -25,9 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		k = j;
 		len += j;
-	}
-
-	else
+	} else
 	{
 		k = n;
 		len += n;
@@ -35,19 +35,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	len++;
 
-	res = malloc(sizeof(*res) * len);
+	res = malloc(sizeof(char) * len);
 
 	if (res == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1 != NULL && s1[i] != '\0'; i++)
 		res[i] = s1[i];
 
-	for (j = 0; j < k; j++)
+	for (j = 0; s2 != NULL && j < k; j++)
 	{
 		res[i] = s2[j];
 		i++;
 	}
 
+	res[i] = '\0';
 	return (res);
 }
