3c3167c64bcbc45998b407cda14aa75e3e2576b1
CyberGA <etomu.joshua@gmail.com>
2023-08-14 19:29:05 -0700

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dog
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: dog's age
+ *
+ * Description: This is a dog object
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+
+#endif /* DOG_H */
