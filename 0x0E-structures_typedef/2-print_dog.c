1baa5fa80b43abd602db4fca7359fa6fab1aed6e
CyberGA <etomu.joshua@gmail.com>
2023-08-14 19:53:51 -0700

    prints a struct data

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a dog's data
+ * @d: dog data
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		d->name == (NULL) ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
+		d->age == (NULL) ? printf("Age: (nil)\n") : printf("Age: %lf\n", d->age);
+		(*d).owner == (NULL) ? printf("Owner: (nil)\n")
+			: printf("Owner: %s\n", (*d).owner);
+	}
+}
