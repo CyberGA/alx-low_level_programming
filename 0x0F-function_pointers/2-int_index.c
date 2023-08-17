8ad31a4c538d308e3f70c5f6b366827f798dac2c
CyberGA <etomu.joshua@gmail.com>
2023-08-16 17:10:17 -0700

    searches for an integer.

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: the elements
+ * @size: lenght of the array
+ * @cmp: callback function
+ *
+ * Return: integer value
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size <= 0)
+		return (-1);
+
+	if (array && cmp)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (array[i]);
+			}
+		}
+	}
+	return (-1);
+}
+
