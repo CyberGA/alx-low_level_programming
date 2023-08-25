50eb7ac6605174b130d324d88171ef11b9ab87f2
CyberGA <etomu.joshua@gmail.com>
2023-08-24 18:49:30 -0700

    prints all the elements of a list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements
+ * @h: head
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *ptr;
+	size_t len = 0;
+
+	ptr = h;
+
+	if (ptr != NULL)
+	{
+		while (ptr->next != NULL)
+		{
+			if (ptr->str == NULL)
+				printf("[0] (nil)\n");
+			else
+				printf("[%zu] %s\n", len, ptr->str);
+			ptr = ptr->next;
+			len++;
+		}
+	}
+	return (len);
+}
