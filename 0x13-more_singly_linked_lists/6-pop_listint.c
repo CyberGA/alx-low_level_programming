a430129c6817b844d7e5622f15b749cdf93c3f9f
CyberGA <etomu.joshua@gmail.com>
2023-08-29 10:48:29 -0700

    delete head node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes head node
+ * @head: address of the head node
+ * Return: head node data
+ */
+int pop_listint(listint_t **head);
+{
+	int data;
+	listint_t *temp;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	data = (*head)->n;
+	temp = (*head)->next
+	free(*head);
+	(*head) = temp;
+
+	return (data);
+}
