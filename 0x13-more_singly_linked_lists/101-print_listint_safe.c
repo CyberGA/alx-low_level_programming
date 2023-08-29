e6717ca6a27b1e08a03e1b46a220aacc4d879802
CyberGA <etomu.joshua@gmail.com>
2023-08-29 15:25:59 -0700

    print lists with a loop

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,14 +7,27 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *temp = head;
-	size_t count = 0;
+	const listint_t *temp = head, *prev;
+	size_t count = 0, i;
 
 	while (temp)
 	{
 		printf("[%p] %d\n", (void *)temp, temp->n);
 		temp = temp->next;
 		count++;
+		prev = head;
+		i = 0;
+
+		while (i < count)
+		{
+			if (temp == prev)
+			{
+				printf("-> [%p] %d\n", (void *)temp, temp->n);
+				return (count);
+			}
+			i++;
+			prev = prev->next;
+		}
 	}
 	return (count);
 }
