d197146ca1be56e9e0da064eaefc690bac3d778a
CyberGA <etomu.joshua@gmail.com>
2023-08-29 11:37:23 -0700

    inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *cur, *new, *next;
-	int i = 0;
+	unsigned int i = 0;
 
 	if (!*head || !head)
 		return (NULL);
