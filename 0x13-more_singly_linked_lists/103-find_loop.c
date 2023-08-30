8808a665082ee423a0d7d1ae805d93d8acd3e547
CyberGA <etomu.joshua@gmail.com>
2023-08-29 18:11:11 -0700

    finds the loop in a linked list.

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -7,25 +7,29 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	const listint_t *temp = head, *prev;
-	size_t count = 0, i;
+	listint_t *temp;
+	listint_t *ptr;
 
-	while (temp)
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	temp = head;
+	ptr = head;
+
+	do {
+		temp = temp->next;
+		ptr = ptr->next;
+		if (ptr == NULL || ptr->next == NULL)
+			return (NULL);
+		ptr = ptr->next;
+	} while (temp != ptr);
+
+	temp = head;
+	while (temp != ptr)
 	{
 		temp = temp->next;
-		count++;
-		prev = head;
-		i = 0;
-
-		while (i < count)
-		{
-			if (temp == prev)
-			{
-				return (temp);
-			}
-			i++;
-			prev = prev->next;
-		}
+		ptr = ptr->next;
 	}
-	return (NULL);
+
+	return (temp);
 }
