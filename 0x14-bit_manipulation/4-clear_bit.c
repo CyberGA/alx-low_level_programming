4a3fafe5f9d359f90ce51eb77bbad3ac04bd9ba2
CyberGA <etomu.joshua@gmail.com>
2023-08-31 16:51:55 -0700

    sets the value of a bit to 0 at a given index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,7 +2,7 @@
 
 /**
  * clear_bit - sets the value of a bit to 0 at a given index
- * @n: decimal
+ * @n: pointer to the decimal
  * @index: index of specific bit
  * Return: 1 if success, -1 if error
  */
@@ -13,14 +13,11 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index > 64)
 		return (-1);
-
 	hold = index;
-
 	for (i = 1; hold > 0; i *= 2, hold--)
-	{
-	}
+		;
 
-	if ((*n >> index) * 1)
+	if ((*n >> index) & 1)
 		*n -= i;
 
 	return (1);
