3223c02fd6985e0607871d598df17ba15310f82b
CyberGA <etomu.joshua@gmail.com>
2023-08-31 16:09:23 -0700

    convert bin to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,7 +7,8 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, res = 0, power = 1;
+	int i;
+	unsigned int res = 0, power = 1;
 
 	if (b == NULL)
 		return (0);
@@ -18,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 			return (0);
 	}
 
-	for (; i >= 0; i--)
+	for (i = i - 1; i >= 0; i--)
 	{
 		if (b[i] == '1')
 			res += power;
