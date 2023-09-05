5515953fc9053991126467005024371e9d5174ba
CyberGA <etomu.joshua@gmail.com>
2023-09-04 19:29:00 -0700

    creates a file and write content to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,18 +8,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fp, bytesWrite;
+	int fp, bytesWrite, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	bytesWrite = write(fp, text_content, strlen(text_content));
+	bytesWrite = write(fp, text_content, len);
 
 	if (fp == -1 || bytesWrite == -1)
 		return (-1);
 
 	close(fp);
-
 	return (1);
 }
