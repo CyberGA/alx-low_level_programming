151db44b5658113c2b2452e33c6f778f6b3ad51e
CyberGA <etomu.joshua@gmail.com>
2023-10-16 11:22:30 -0700

    feature: dynamic library

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_len, src_len;
+
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+	{
+	}
+
+	for (src_len = 0; src[src_len] != '\0'; src_len++)
+	{
+		dest[dest_len + src_len] = src[src_len];
+	}
+	return (dest);
+}
