7486a4894230ac326983f3b61519f68a5a1f5e8a
CyberGA <etomu.joshua@gmail.com>
2023-11-03 15:45:25 -0700

    fix: hash table insertion

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,51 +1,53 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Add or update an element in the hash table
- * @ht: pointer to the hash table
- * @key: The key to add, cannot be empty
- * @value: value associated with key
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
  *
- * Return: 0 on failure, else 1 on success
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
-	char *val;
-	unsigned long int index;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	val = strdup(value);
-	if (val == NULL)
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 		return (0);
+
 	index = key_index((const unsigned char *)key, ht->size);
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	for (i = index; ht->array[i]; i++)
 	{
-		free(val);
-		return (0);
-	}
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(val);
-		free(new_node);
-		return (0);
-	}
-	new_node->value = val;
-	if (ht->array[index] == NULL)
-	{
-		new_node->next = NULL;
-		ht->array[index] = new_node;
-	}
-	else
-	{
-		if (strcmp(ht->array[index]->key, key) == 0)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			new_node->next = ht->array[index];
-			ht->array[index] = new_node;
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
 		}
 	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
 	return (1);
 }
